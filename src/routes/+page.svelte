1733792c9a04661a447fbd0f8e70bd8a3775618a
georgebdev <46300928+georgebdev@users.noreply.github.com>
2023-01-21 20:11:10 +0000

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	const date = new Date();
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte app" />
+</svelte:head>
+
+<section> 	
+	<h1 class="text-teal-700">
+		{ "Good " + (date.getHours() < 12 ? "Morning" : (date.getHours() < 17 ? "Afternoon" : "Evening")) }
+	</h1>
+	<hr />
+	<h3 class="text-teal-800 font-semibold">It is currently { date.getHours() }:{ date.getMinutes() } &bull; { date.getDate() }/{ date.getMonth() + 1 }/{ date.getFullYear() }</h3>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
